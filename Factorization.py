3423e143bd33f398d19b929a7db46fb53b0e3692
delgaka <kayodelgado@gmail.com>
2017-02-06 22:33:25 -0200

    Conversion to python 3

--- Factorization.py
+++ Factorization.py
@@ -13,11 +13,24 @@ https://github.com/delgaka
 """
 
 
+# convert input to a number
+def input_to_number(user_input):
+    if is_number(user_input):
+        # if the float number less your int conversion is greater than 0.0 is a float number
+        if (float(user_input) - int(float(user_input))) > 0.0:
+            # we don't want a float number to iterate
+            return False
+        else:
+            # in python3 we need to convert a explicit float to an int number
+            return int(float(user_input))
+    return False
+
+
 # Check if the variable is a number (float or integer)
-def is_number(number):
+def is_number(user_number):
     try:
-        float(number)
-        int(number)
+        # I only need to test a float conversion
+        float(user_number)
         return True
     except ValueError:
         return False
@@ -35,16 +48,19 @@ def is_factor(number, n):
 def look_for_factor(number):
     for i in range(1, int(number) + 1):
         if is_factor(number, i):
-            print "%s is factor." % i
-        else:
-            print "%i is not a factor." % i
+            print("%s is factor." % i)
+
+
+def main():
+    num = input("Please, give an integer number: ")
+    user_number = input_to_number(num)
+    # if the input is not a number, the program will as to run again with a number.
+    if is_number(num) and user_number:
+        look_for_factor(user_number)
+    else:
+        print("Please, try again with an integer number!")
 
 
 if __name__ == '__main__':
-    num = input("Please, give a number: ")
-    # if the input is not a number, the program will as to run again with a number.
-    if is_number(num):
-        look_for_factor(num)
-    else:
-        print "Please, try again with a number!"
+    main()
 
